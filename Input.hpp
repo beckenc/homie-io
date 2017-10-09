df3e2a2444246650cdae467181b872abbbebb44d
CBe <github@claus-beckenbauer.de>
2017-10-09 06:48:49 +0200

    Add files via upload

---
+++ Input.hpp
@@ -0,0 +1,53 @@
+#ifndef _INPUT_NODE
+#define _INPUT_NODE
+
+class Input : HomieNode {
+  public:
+    Input(int input, const char* id);
+    virtual ~Input();
+
+    struct state {
+      unsigned long millis;
+      bool state;
+    };
+
+    void setup();
+    state current() const;
+    state last() const;
+    bool publish();
+ 
+ private:
+    int pin;
+    state lastState;
+};
+
+inline Input::Input(int input, const char* id)
+  : HomieNode(id, "input")
+  , pin(input) {
+  pinMode(pin, INPUT_PULLUP);
+  lastState.state = digitalRead(pin) == 0;
+  lastState.millis = 0;
+}
+
+inline Input::~Input() {
+}
+
+inline void Input::setup() {
+  advertise("state");
+}
+
+Input::state Input::current() const {
+  return { millis(), digitalRead(pin) == 0 };
+}
+
+Input::state Input::last() const {
+  return lastState;
+}
+
+bool Input::publish() {
+  lastState.state = digitalRead(pin) == 0;
+  lastState.millis = millis();
+  return setProperty("state").setRetained(true).send(String(lastState.state)) != 0;
+}
+
+#endif
---
+++ homie-io.ino
@@ -0,0 +1,79 @@
+#include <Homie.hpp>
+#include <vector>
+#include "Input.hpp"
+
+#define _DEBUG
+
+static HomieSetting<long> publishIntervalSetting("publishInterval", "publish interval in seconds");
+static HomieSetting<bool> deepSleepSetting("deepSleep", "ESP deep sleep mode");
+static std::vector<Input*> inputs;
+
+static void setupHandler() {
+  Homie.getLogger() << "homie-io - " << __DATE__ << " - " << __TIME__ << endl;
+  for (std::vector<Input*>::iterator input = inputs.begin() ; input != inputs.end(); ++input) {
+    (*input)->setup();
+  }
+}
+
+static void loopHandler() {
+  const bool deepSleep = deepSleepSetting.get();
+  const long publishInterval = publishIntervalSetting.get();
+  bool published = true;
+
+  for (std::vector<Input*>::iterator input = inputs.begin() ; input != inputs.end(); ++input) {
+    Input::state last = (*input)->last();
+    Input::state current = (*input)->current();
+    if (((current.millis - last.millis) >= (publishInterval * 1000UL)) || (last.state != current.state || (last.millis == 0)) ) {
+      published &= (*input)->publish();
+    }
+  }
+
+  if (deepSleep) {
+    // publishing successful. Go into deep sleep.
+#ifndef _DEBUG
+    Homie.getLogger() << "Preparing for deep sleep (" << publishInterval << " seconds)" << endl;
+    Homie.prepareToSleep();
+#endif
+  }
+}
+
+void onHomieEvent(const HomieEvent & event) {
+  switch (event.type) {
+    case HomieEventType::READY_TO_SLEEP:
+      Homie.getLogger() << "Ready to sleep" << endl;
+      // convert to microseconds
+      ESP.deepSleep(publishIntervalSetting.get() * 1000000);
+      break;
+  }
+}
+
+void setup() {
+  // Connect D0 to RST to wake up
+  pinMode(D0, WAKEUP_PULLUP);
+
+  Serial.begin(115200);
+  Serial << endl << endl;
+
+  Homie_setFirmware("homie-io", "0.0.1");
+  Homie_setBrand("homie-io");
+  Homie.setSetupFunction(setupHandler).setLoopFunction(loopHandler).onEvent(onHomieEvent);
+  Homie.disableLedFeedback();
+  Homie.disableResetTrigger();
+
+  deepSleepSetting.setDefaultValue(false);
+  publishIntervalSetting.setDefaultValue(300UL).setValidator([] (long candidate) {
+    return (candidate >= 2) && (candidate < 86400); // 2sec - 1day
+  });
+
+  // todo take the config from a HomieSettings Object
+  inputs.push_back(new Input(D1, "in[0]"));
+  inputs.push_back(new Input(D2, "in[1]"));
+  inputs.push_back(new Input(D6, "in[2]"));
+  inputs.push_back(new Input(D7, "in[3]"));
+
+  Homie.setup();
+}
+
+void loop() {
+  Homie.loop();
+}
